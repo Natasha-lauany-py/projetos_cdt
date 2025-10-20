4526375b3ea8b60ddb76fc58f14a6a118b5b7c34
Blessed <zsblessed011@gmail.com>
2025-10-20 17:00:00 -0300

    atividade do dia 20
     segue atividade feita em sala de aula escholido a *lista de desejo* e/ou site em pyton*
    (atividade deve ser entregue no dia 03*11)

---
+++ consequencia/miguel_consequencia.py
@@ -0,0 +1,11 @@
+'''
+primicias 
+jogo da memoria
+ele vai conter buttons 
+e será um jogo bem divertido
+e pratico de se jogar, nesse codigo 
+voce vai ver uma logica de programaçao 
+otima.
+------> jogo da memoria <---------
+
+'''
